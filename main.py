3db579aa86faebb711cb8348bc66860b37cdfa05
clinteads <clint.eads@gmail.com>
2025-12-31 16:54:08 -0700

    file read working

---
+++ main.py
@@ -0,0 +1,10 @@
+def get_book_text(filepath):
+    with open(filepath) as file:
+        file_contents=file.read()
+    return file_contents
+
+def main():
+    book_name = "books/frankenstein.txt"
+    print(get_book_text(book_name))
+
+main()
