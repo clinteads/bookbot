2b471504d953653052798b87b7f1662d4623414e
clinteads <clint.eads@gmail.com>
2025-12-31 18:22:05 -0700

    updated main

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+from stats import count_words
+
 def get_book_text(filepath):
     with open(filepath) as file:
         file_contents=file.read()
     return file_contents
 
-def count_words(text):
-    return len(text.split())
+
 
 def main():
     book_name = "books/frankenstein.txt"
