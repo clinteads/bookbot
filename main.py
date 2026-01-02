b79cc60efcb0c7359044200f63cc7318f37e0d6f
clinteads <clint.eads@gmail.com>
2026-01-01 18:32:46 -0700

    change book from hard coded to argument

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words, count_letters, generate_report
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as file:
@@ -8,7 +9,11 @@ def get_book_text(filepath):
 
 
 def main():
-    book_name = "books/frankenstein.txt"
+    if len(sys.argv) == 2:
+        book_name = sys.argv[1]
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     book_data = get_book_text(book_name)
     num_words = count_words(book_data)
     num_letters = count_letters(book_data)
